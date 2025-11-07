3dfabb513f101b7f5dca7c05bfb9848c9d43c879
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-11-06 22:58:47 -0800

    refactor(assets): group images into senate and decoration folders

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,27 +1,30 @@
-import abigailVerino from "./abigail-verino.png";
-import asucLogo from "./asuc-logo.png";
-import asucSenatorThyColorLogo from "./asuc-senator-thy-color-logo.png";
-import asucSenatorVerinoBlackLogo from "./asuc-senator-verino-black-logo.png";
-import lotusFlower from "./lotus-flower.jpg";
-import senateLogo from "./senate-logo.png";
-import sohotonCoveBucasGrandePhilippines from "./sohoton-cove-bucas-grande-philippines.jpg";
-import sydneeThy from "./sydnee-thy.jpeg";
-import tegallalangRiceTerracesBaliLandscape from "./tegallalang-rice-terraces-bali-landscape.jpg";
-import tegallalangRiceTerracesBaliOld from "./tegallalang-rice-terraces-bali-old.png";
-import tegallalangRiceTerracesBaliPortrait from "./tegallalang-rice-terraces-bali-portrait.jpg";
-import theEmpireBruneiJerudongDay from "./the-empire-brunei-jerudong-day.webp";
-import theEmpireBruneiJerudongNight from "./the-empire-brunei-jerudong-night.jpeg";
-import theEmpireBruneiJerudongOld from "./the-empire-brunei-jerudong-old.png";
+import asucLogo from "./senate/asuc-logo.png";
+import senateLogo from "./senate/senate-logo.png";
+
+import abigailVerino from "./senate/abigail-verino.png";
+import asucSenatorVerinoBlackLogo from "./senate/asuc-senator-verino-black-logo.png";
+
+import sydneeThy from "./senate/sydnee-thy.jpeg";
+import asucSenatorThyColorLogo from "./senate/asuc-senator-thy-color-logo.png";
+
+import lotusFlower from "./decoration/lotus-flower.jpg";
+import sohotonCoveBucasGrandePhilippines from "./decoration/sohoton-cove-bucas-grande-philippines.jpg";
+import tegallalangRiceTerracesBaliLandscape from "./decoration/tegallalang-rice-terraces-bali-landscape.jpg";
+import tegallalangRiceTerracesBaliOld from "./decoration/tegallalang-rice-terraces-bali-old.png";
+import tegallalangRiceTerracesBaliPortrait from "./decoration/tegallalang-rice-terraces-bali-portrait.jpg";
+import theEmpireBruneiJerudongDay from "./decoration/the-empire-brunei-jerudong-day.webp";
+import theEmpireBruneiJerudongNight from "./decoration/the-empire-brunei-jerudong-night.jpeg";
+import theEmpireBruneiJerudongOld from "./decoration/the-empire-brunei-jerudong-old.png";
 
 export {
-  abigailVerino,
   asucLogo,
-  asucSenatorThyColorLogo,
-  asucSenatorVerinoBlackLogo,
-  lotusFlower,
   senateLogo,
-  sohotonCoveBucasGrandePhilippines,
+  abigailVerino,
+  asucSenatorVerinoBlackLogo,
   sydneeThy,
+  asucSenatorThyColorLogo,
+  lotusFlower,
+  sohotonCoveBucasGrandePhilippines,
   tegallalangRiceTerracesBaliLandscape,
   tegallalangRiceTerracesBaliOld,
   tegallalangRiceTerracesBaliPortrait,
