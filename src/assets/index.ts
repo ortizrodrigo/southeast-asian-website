29fa15389d8749f67a41595f46019aa1bc08b409
Rodrigo Ortiz <rodrigo.ortiz@berkeley.edu>
2025-11-06 22:46:22 -0800

    feat(assets): add images and central index.ts export file for easier imports

---
+++ src/assets/index.ts
@@ -0,0 +1,31 @@
+import abigailVerino from "./abigail-verino.png";
+import asucLogo from "./asuc-logo.png";
+import asucSenatorThyColorLogo from "./asuc-senator-thy-color-logo.png";
+import asucSenatorVerinoBlackLogo from "./asuc-senator-verino-black-logo.png";
+import lotusFlower from "./lotus-flower.jpg";
+import senateLogo from "./senate-logo.png";
+import sohotonCoveBucasGrandePhilippines from "./sohoton-cove-bucas-grande-philippines.jpg";
+import sydneeThy from "./sydnee-thy.jpeg";
+import tegallalangRiceTerracesBaliLandscape from "./tegallalang-rice-terraces-bali-landscape.jpg";
+import tegallalangRiceTerracesBaliOld from "./tegallalang-rice-terraces-bali-old.png";
+import tegallalangRiceTerracesBaliPortrait from "./tegallalang-rice-terraces-bali-portrait.jpg";
+import theEmpireBruneiJerudongDay from "./the-empire-brunei-jerudong-day.webp";
+import theEmpireBruneiJerudongNight from "./the-empire-brunei-jerudong-night.jpeg";
+import theEmpireBruneiJerudongOld from "./the-empire-brunei-jerudong-old.png";
+
+export {
+  abigailVerino,
+  asucLogo,
+  asucSenatorThyColorLogo,
+  asucSenatorVerinoBlackLogo,
+  lotusFlower,
+  senateLogo,
+  sohotonCoveBucasGrandePhilippines,
+  sydneeThy,
+  tegallalangRiceTerracesBaliLandscape,
+  tegallalangRiceTerracesBaliOld,
+  tegallalangRiceTerracesBaliPortrait,
+  theEmpireBruneiJerudongDay,
+  theEmpireBruneiJerudongNight,
+  theEmpireBruneiJerudongOld,
+};
